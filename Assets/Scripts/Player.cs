6b51f64253749ba6507545c28c9e2f4a35eb3967
p86963sp <samkitt.patni@student.manchester.ac.uk>
2024-07-16 18:07:23 +0100

    Fixed Jump and added Dash Functionality

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,17 +6,27 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private Rigidbody2D rb;
-    [SerializeField]
-    private float _speed = 5f;
-    [SerializeField]
-    private float _jump = 600f;
+    [SerializeField] private float _speed = 10f;
+
+    [SerializeField] private float _dashSpeed = 20f;
+    [SerializeField] private float _dashingTime = 0.2f;
+    private bool canDash = true;
+
+    private float _fallMultiplier = 5f;
+    private float _lowJumpMultiplier = 2f;
+    [SerializeField] private float _jump = 18f;
+
+    [SerializeField] private float _direction = -1f;
+
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private TrailRenderer tr;
 
     private bool onGround;
     void Start()
     {
         onGround = false;
         rb = GetComponent<Rigidbody2D>();
+        tr = GetComponent<TrailRenderer>();
     }
 
     // Update is called once per frame
@@ -25,15 +35,42 @@ public class Player : MonoBehaviour
         // left and right movement
         float horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector2.right * horizontalInput * _speed * Time.deltaTime);
-
-        // Jump functionality if player is on the ground
+        if (Input.GetButtonDown("Fire3") && canDash)
+        {
+            StartCoroutine(Dash());
+            //rb.AddForce(new Vector2(_dashSpeed, 0));
+        }
         if (onGround)
         {
-            if (Input.GetButtonDown("Jump"))
-            {
-                rb.AddForce(new Vector2(rb.velocity.x, _jump));
-            }
+            canDash = true;
         }
+        // Jump functionality if player is on the ground
+        if (Input.GetButtonDown("Jump") && onGround)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, _jump);
+        }
+
+        // Altering gravity for faster fall
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (_fallMultiplier - 1) * Time.deltaTime;
+        }
+        // Allowing for different jump heights
+        else if (rb.velocity.y > 0 && !Input.GetButton("Jump"))
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (_lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+
+        // Finding player direction
+        if (Input.GetKeyDown("a"))
+        {
+            _direction = -1f;
+        }
+        if (Input.GetKeyDown("d"))
+        {
+            _direction = 1f;
+        }
+
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -50,6 +87,31 @@ public class Player : MonoBehaviour
         if (collision.gameObject.tag == "Ground")
         {
             onGround = false;
+        }
+    }
+
+    private IEnumerator Dash()
+    {
+        // Setting up dash
+        canDash = false;
+        float originalGravity = rb.gravityScale;
+        rb.gravityScale = 0f;
+        tr.emitting = true;
+
+        // Dash
+        rb.velocity = new Vector2(_direction * _dashSpeed, 0f);
+        yield return new WaitForSeconds(_dashingTime);
+
+        // Resetting parameters
+        rb.gravityScale = originalGravity;
+        tr.emitting = false;
+        rb.velocity = new Vector2(0f, 0f);
+
+        // Dash Cooldown
+        yield return new WaitForSeconds(_dashingTime);
+        if (onGround)
+        {
+            canDash = true;
         }
     }
 }
